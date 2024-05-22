13aa8245ce87e24c2724c61c5676b830807294d0
dawei <chenjianwei01@baidu.com>
2024-05-22 15:45:37 +0800

    feat: 自定义constom hooks

--- src/pages/projectList/index.jsx
+++ src/pages/projectList/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import * as qs from "qs";
 import { Search } from "./search";
 import { List } from "./list";
-import { emptyObj } from "../../utils";
+import { emptyObj, useDebounce, useMount } from "../../utils";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectList = () => {
@@ -12,26 +12,27 @@ export const ProjectList = () => {
         name: "",
         personId: "",
     });
+    const debounceParam = useDebounce(param, 2000);
     const [list, setList] = useState([]);
     // 参数变化的时候 获取用户列表
     useEffect(() => {
         // 提前这个地址
         fetch(
             // 拼接参数 如果参数很多很麻烦，可以使用qs库  `${apiUrl}/projects?name=${param.name}&personId=${param.personId}`,
-            `${apiUrl}/projects?${qs.stringify(emptyObj(param))}`,
+            `${apiUrl}/projects?${qs.stringify(emptyObj(debounceParam))}`,
         )
             .then((res) => res.json())
             .then((data) => {
                 setList(data);
             });
-    }, [param]);
-    useEffect(() => {
+    }, [debounceParam]); // 其实是监听的 debounceParam的值的变化的频率！！！！
+    useMount(() => {
         fetch(`${apiUrl}/users`)
             .then((res) => res.json())
             .then((data) => {
                 setUsers(data);
             });
-    }, []);
+    }); // 第一次加载的时候执行一次   初始化的情况，是非常多的
     return (
         <div>
             <Search users={users} param={param} setParam={setParam} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 export const isEmpty = (value) => (value === 0 ? false : !value);
 // 函数内部 不要改变传入的对象， 不要污染传入的对象
 export const emptyObj = (obj) => {
@@ -9,4 +10,25 @@ export const emptyObj = (obj) => {
         }
     });
     return res;
+};
+
+export const useMount = (fn) => {
+    // 必须要在hooks里面使用， 去掉 use eslit报错， 所有一定要 use开头
+    useEffect(() => {
+        fn();
+    }, []);
+};
+
+export const useDebounce = (value, delay) => {
+    const [debounceValue, setDebounceValue] = useState(value);
+    useEffect(() => {
+        // 每次value改变的时候， 重新设置 定时器， debounceValue
+        const timer = setTimeout(() => {
+            setDebounceValue(value);
+        }, delay);
+        // 每次在上次useEffect执行后 (一般都是负责清理的任务) 再执行这个return后的内容，  每次执行完上一次的时候， 清除定时器
+        return () => clearTimeout(timer); // 最后一个没人处理， 定时器没有被清除
+    }, [value, delay]);
+
+    return debounceValue;
 };
